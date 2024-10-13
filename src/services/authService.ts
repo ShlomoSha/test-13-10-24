19e16a97ccf2c00342d0fcdff3ed72a283279c8c
shlomoShalem <s0552288304@gmail.com>
2024-10-13 14:52:31 +0300

    auth service done

---
+++ src/services/authService.ts
@@ -0,0 +1,31 @@
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import UserModel from '../types/schenas/userModel'
+import IUser from '../types/interfaces/iuser'
+
+export const login = async (user: IUser): Promise<string | void> => {
+    try {
+        const dbUser = await UserModel.findOne({name: user.name})
+
+        if (!dbUser) throw new Error('user nor found')
+        if (!(await bcrypt.compare(user.password, dbUser.password))){
+            throw new Error('something wrong')
+        }
+
+        const token: string = await jwt.sign(
+            {
+                id: dbUser._id,
+                name: dbUser.name,
+                role: dbUser.role,
+                class: dbUser.class
+            },
+            process.env.TOKEN_SECRET!, 
+            {
+                expiresIn: '10m'
+            }
+        )
+        return token
+    } catch (err) {
+        throw err
+    }
+}
