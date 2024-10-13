f6f5c7e104b61bacbc044e511158809f0d6e5b56
shlomoShalem <s0552288304@gmail.com>
2024-10-13 11:43:57 +0300

    added swagger config withuot apis

--- src/config/swaggerCongig.ts
+++ src/config/swaggerCongig.ts
@@ -1,15 +1,15 @@
-import swaggerJsdoc from "swagger-jsdoc"
+import swaggerJsdoc from "swagger-jsdoc";
 
 const options = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'Green Eye',
-            version: '1.0.0',
-            descriptio: 'A simple Green Eye Express API'
-        },
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Green Eye",
+      version: "1.0.0",
+      descriptio: "A simple Green Eye Express API",
     },
-    apis: []
-}
+  },
+  apis: [],
+};
 
-export const specs = swaggerJsdoc(options)
+export const specs = swaggerJsdoc(options);
