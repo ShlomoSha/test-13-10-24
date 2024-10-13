f9fc2d2cf79329ef4e00baead99105e2f1605125
shlomoShalem <s0552288304@gmail.com>
2024-10-13 11:13:50 +0300

    DB CONFIG DONE

---
+++ src/config/dbConnect.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose"
+
+const connectToMongo = async ()=> {
+    try {
+        await mongoose.connect(process.env.MONGO_PATH!)
+        console.log('success connected to MongDB')
+    } catch (err) {
+        console.log(err)
+    }
+}
